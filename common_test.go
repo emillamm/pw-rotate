8f3beb606978f2d01ef9927427adfd29c51a48c5
Emil Lamm Nielsen <e@lamm.io>
2024-06-04 18:04:56 +0200

    refactor tests

---
+++ common_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"testing"
+)
+
+// Test a Rotator
+func RotatorTest(t *testing.T, r Rotator, user string, oldPw string, newPw string) {
+	t.Helper()
+
+	t.Run("authentication using oldPw should succeed and newPw should fail", func(t *testing.T) {
+		if err := r.Ping(user, oldPw); err != nil {
+			t.Errorf("authentication with oldPw %s not successful: %#v", oldPw, err)
+		}
+		if err := r.Ping(user, newPw); err == nil {
+			t.Errorf("authentication with newPw %s was successful", newPw)
+		}
+	})
+
+	t.Run("rotate should update oldPw to newPw", func(t *testing.T) {
+		if err := r.Rotate(user, oldPw, newPw); err != nil {
+			t.Errorf("failed to rotate passwords: %s", err)
+		}
+	})
+
+	t.Run("authentication using oldPw should fail and newPw should succeed", func(t *testing.T) {
+		if err := r.Ping(user, oldPw); err == nil {
+			t.Errorf("authentication with oldPw %s was successful: %#v", oldPw, err)
+		}
+		if err := r.Ping(user, newPw); err != nil {
+			t.Errorf("authentication with newPw %s not successful", newPw)
+		}
+	})
+
+}
+
--- postgres_rotator_test.go
+++ postgres_rotator_test.go
@@ -40,37 +40,8 @@ func TestPostgresRotator(t *testing.T) {
 	ephemeralPostgresUser(t, db, tmpPassword, func(tmpUser string) {
 		rotator := PostgresRotator{Host : host, Port: port}
 		// run tests
-		TRotator(t, rotator, tmpUser, tmpPassword, newPassword)
+		RotatorTest(t, rotator, tmpUser, tmpPassword, newPassword)
 	})
-}
-
-func TRotator(t *testing.T, r Rotator, user string, oldPw string, newPw string) {
-	t.Helper()
-
-	t.Run("authentication using oldPw should succeed and newPw should fail", func(t *testing.T) {
-		if err := r.Ping(user, oldPw); err != nil {
-			t.Errorf("authentication with oldPw %s not successful: %#v", oldPw, err)
-		}
-		if err := r.Ping(user, newPw); err == nil {
-			t.Errorf("authentication with newPw %s was successful", newPw)
-		}
-	})
-
-	t.Run("rotate should update oldPw to newPw", func(t *testing.T) {
-		if err := r.Rotate(user, oldPw, newPw); err != nil {
-			t.Errorf("failed to rotate passwords: %s", err)
-		}
-	})
-
-	t.Run("authentication using oldPw should fail and newPw should succeed", func(t *testing.T) {
-		if err := r.Ping(user, oldPw); err == nil {
-			t.Errorf("authentication with oldPw %s was successful: %#v", oldPw, err)
-		}
-		if err := r.Ping(user, newPw); err != nil {
-			t.Errorf("authentication with newPw %s not successful", newPw)
-		}
-	})
-
 }
 
 func openConnection(connStr string) (db *sql.DB, err error) {
